898bf151a4210d96b459385c06166af4b7426924
pcubewg <pcube.writers.group@gmail.com>
2023-01-19 19:45:58 +0530

    Pcube Contents

--- script.js
+++ script.js
@@ -21,21 +21,22 @@ function log(f) {
       if (this.readyState == 4 && this.status == 200) {
         console.log("verifying...");
         var emails = [];
-        for (let i=0;i<JSON.parse(atob(this.responseText)).length;i++){
+        for (let i = 0; i < JSON.parse(atob(this.responseText)).length; i++) {
           var d = JSON.parse(atob(this.responseText));
           emails.push(d[i].email);
         }
 
-        if(document.getElementById(email).value in emails){
+        if (document.getElementById(email).value in emails) {
           emails = emails.findIndex(document.getElementById(email).value);
         }
-        if(d[emails].pass === document.getElementById("password").value){
-          sessionStorage.setItem('edata',[d[emails].name,d[emails].email]);
+        if (d[emails].pass === document.getElementById("password").value) {
+          sessionStorage.setItem('edata', [d[emails].name, d[emails].email]);
           window.location.replace("https://pcubewg.github.io/testpage.html")
           window.location.href = "https://pcubewg.github.io/testpage.html"
-      }
-    };
-    xhttp.open("GET", "emp.data", true);
-    xhttp.send();
+        }
+      };
+      xhttp.open("GET", "emp.data", true);
+      xhttp.send();
+    }
   }
 }
